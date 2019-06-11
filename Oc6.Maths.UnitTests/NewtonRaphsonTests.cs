a81f59812115709fb82b4f3c7cc8d2b3c4003038
Marc Barnholdt <marc@leneo.io>
2019-06-11 08:53:53 +0200

    newton raphson tests

---
+++ Oc6.Maths.UnitTests/NewtonRaphsonTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Oc6.Maths.Shared;
+using System;
+
+namespace Oc6.Maths.UnitTests
+{
+    [TestClass]
+    public class NewtonRaphsonTests
+    {
+        [TestMethod]
+        public void Iterate_IterationsExceededException()
+        {
+            Func<double, double> fx = x => Math.Pow(x, 8) - 100.0;
+            Func<double, double> dydx = x => 8 * Math.Pow(x, 7);
+            Assert.ThrowsException<IterationsExceededException<double>>(() => NewtonRaphson.Iterate(fx, dydx, iterations: 10));
+        }
+
+        [TestMethod]
+        public void Iterate()
+        {
+            Func<double, double> fx = x => Math.Pow(x, 8) - 100.0;
+            Func<double, double> dydx = x => 8 * Math.Pow(x, 7);
+            double actual = NewtonRaphson.Iterate(fx, dydx);
+            double expected = 1.77827941003892;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
