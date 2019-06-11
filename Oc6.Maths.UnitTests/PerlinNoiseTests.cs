1b8d37b4f0976e9d438cbb549d7d7b4cee17ffd5
Marc Barnholdt <marc@home>
2019-06-11 22:57:59 +0200

    PerlinNoise

---
+++ Oc6.Maths.UnitTests/PerlinNoiseTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Oc6.Maths.UnitTests
+{
+    [TestClass]
+    public class PerlinNoiseTests
+    {
+        [TestMethod]
+        public void Noise()
+        {
+            const double expected = 0.13691995878400012;
+            double actual = PerlinNoise.Noise(3.14, 42, 7);
+            Assert.AreEqual<double>(expected, actual);
+        }
+    }
+}
